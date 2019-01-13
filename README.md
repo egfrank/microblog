9c120ace449bba37a506c712cb35af2014e44a4e
egfrank <egfrank@umd.edu>
2019-01-13 15:08:23 -0500

    completmplete pagination chpt 9

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 1/12/19 Got up to this part:
 https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-vii-error-handling
 
-I'm skipping the emailing error portion
 
 
 Working on this part:
 https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-viii-followers
-
 I should go back and re-read the follower / followed data structure
+
+I'm skipping Chapter 10 because it builds on an earlier email part that I skipped. I'll return to these later.
+
+
--- app/forms.py
+++ app/forms.py
@@ -26,7 +26,6 @@ class RegistrationForm(FlaskForm):
       if user is not None:
           raise ValidationError('Please use a different email address.')
 
-
 class EditProfileForm(FlaskForm):
   username = StringField('Username', validators=[DataRequired()])
   about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
@@ -41,3 +40,19 @@ class EditProfileForm(FlaskForm):
         user = User.query.filter_by(username=self.username.data).first()
         if user is not None:
             raise ValidationError('Please use a different username.')
+
+class PostForm(FlaskForm):
+  post = TextAreaField('Say something', validators=[
+        DataRequired(), Length(min=1, max=140)])
+  submit = SubmitField('Submit')
+  
+
+
+
+
+
+  
+
+
+
+
